9846770c04d5888fab3d665ba12c354fc1323fbe
Suraj Namdev gitte <158551269+suraj126708@users.noreply.github.com>
2025-03-30 14:09:08 +0530

    Delete server directory

--- server/models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-const Mongo_url = process.env.Mongo_url;
-
-mongoose
-  .connect(Mongo_url)
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log("mongodb connection err" + err));
