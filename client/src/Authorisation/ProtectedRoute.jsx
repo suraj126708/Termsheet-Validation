1fb1e6e7f90a9bcbe7b18f9801bda6603c9135b7
suraj126708 <158551269+suraj126708@users.noreply.github.com>
2025-04-02 11:16:03 +0530

    loader added

--- client/src/Authorisation/ProtectedRoute.jsx
+++ client/src/Authorisation/ProtectedRoute.jsx
@@ -8,8 +8,11 @@ const ProtectedRoute = ({ children }) => {
 
   if (isAuthenticated === null) {
     return (
-      <div className="h-screen w-full items-center justify-center justify-items-center text-center">
-        Loading...
+      <div className="h-screen w-full flex items-center justify-center">
+        <div className="flex flex-col items-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid"></div>
+          <p className="mt-4 text-gray-600">Loading...</p>
+        </div>
       </div>
     );
   }
